bfcb8f6c08e5783b924b685ee7dbc73e11312479
Blayze <blayzestone@protonmail.com>
2020-05-21 14:28:50 -0700

    create ToggleDarkMode component
    The component is styled by the 'useNavStyles' jss hook.
    needs access to state from App as a prop to toggle dark mode.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "react-jss";
 import { colorMode } from "./utils/colorMode";
+import ToggleDarkMode from "./components/ToggleDarkMode";
 
 function App() {
   const [darkMode, setDarkmode] = useState(true);
 
   return (
     <ThemeProvider theme={() => colorMode(darkMode)}>
-      <div
-        style={{
-          fontSize: "3rem",
-          color: darkMode ? "white" : "black",
-          backgroundColor: darkMode ? "black" : "white",
-        }}
-      >
-        Hello World
+      <div className="App">
+        <ToggleDarkMode darkMode={darkMode} setDarkMode={setDarkmode} />
+        <div
+          style={{
+            fontSize: "3rem",
+            color: darkMode ? "white" : "black",
+            backgroundColor: darkMode ? "black" : "white",
+          }}
+        >
+          Hello World
+        </div>
       </div>
     </ThemeProvider>
   );
