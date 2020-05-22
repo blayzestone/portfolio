527d1a0cb0d61a1432d2e00b32386201d5dc27a0
Blayze <blayzestone@protonmail.com>
2020-05-21 20:25:19 -0700

    create and style Navigation component

--- src/hooks/styles/useStyles.js
+++ src/hooks/styles/useStyles.js
@@ -1,6 +1,36 @@
 import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles((theme) => ({
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    padding: "1rem",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  navigation: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+
+    "@media (min-width: 800px)": {
+      width: "50%",
+    },
+
+    "@media (min-width: 1200px)": {
+      width: "33%",
+    },
+
+    "& a": {
+      fontSize: "1.2rem",
+      color: theme.textColor,
+      textDecoration: "none",
+    },
+  },
+
   section: {
     width: "100%",
     height: "100vh",
