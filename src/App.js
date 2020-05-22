470c87732253887cb129bdf8a18dcc34f141aae9
Blayze <blayzestone@protonmail.com>
2020-05-21 21:40:19 -0700

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import { ThemeProvider } from "react-jss";
 import { colorMode } from "./utils/colorMode";
 import Navigation from "./components/Navigation";
-import ToggleDarkMode from "./components/ToggleDarkMode";
 import Landing from "./sections/Landing";
 import Projects from "./sections/Projects";
-import Skills from "./sections/Skills";
-import About from "./sections/About";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
