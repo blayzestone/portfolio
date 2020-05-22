6cbca50b33d890729283aabec164032edb7c83a6
Blayze <blayzestone@protonmail.com>
2020-05-21 20:42:41 -0700

    create empty components for all sections
    rendering the empty components in App with some text to unsure the
    navigation links scroll to them correctly.

---
+++ src/sections/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Section from "../components/Section";
+import { links } from "../constants";
+
+const About = () => {
+  return (
+    <Section id={links.about}>
+      <div>About</div>
+    </Section>
+  );
+};
+
+export default About;
---
+++ src/sections/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Section from "../components/Section";
+import { links } from "../constants";
+
+const Projects = () => {
+  return (
+    <Section id={links.projects}>
+      <div>Projects</div>
+    </Section>
+  );
+};
+
+export default Projects;
