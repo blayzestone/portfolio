994e0d752f50870f47d56c602ca7393328d24501
Blayze <blayzestone@protonmail.com>
2020-05-21 23:05:15 -0700

    hotfix: fix link to Charity Finder api

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const projects = [
     title: "Charity Finder",
     desc:
       "A front end project that connects with an api to fetch a list of charities. The user can login, search for charities, and save them to their profile. Data is saved to localStorage.",
-    deployed: "charity-finder-27gb3nb1h.now.sh",
+    deployed: "https://charity-finder.now.sh/",
     github: "https://github.com/blayzestone/Charity-Finder",
   },
 ];
