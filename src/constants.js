8cc21d162c7bad181cd2bbcc9d2bf25076fee4c9
Blayze <blayzestone@protonmail.com>
2021-02-03 23:29:41 -0800

    add project data

--- src/constants.js
+++ src/constants.js
@@ -7,9 +7,21 @@ export const links = {
 
 export const projects = [
   {
-    title: "Dev Queue",
-    desc: "a CRUD application that allows students to create tickets and helpers to resolve those tickets.",
-    deployed: "https://dev-queue-2.netlify.app/",
-    github: "https://github.com/Build-Week-Dev-Queue-2/front-end",
+    title: "The Game of Life",
+    desc: "Conway's Game of Life created in React",
+    deployed: "https://blayzestone.github.io/game-of-life/",
+    github: "https://github.com/blayzestone/game-of-life",
+  },
+  {
+    title: "Responsive Site",
+    desc: "Fully responsive, pixel-perfect website created from photoshop mockups",
+    deployed: "https://blayzestone.github.io/Sprint-Challenge--User-Interface/index.html",
+    github: "https://github.com/blayzestone/Sprint-Challenge--User-Interface",
+  },
+  {
+    title: "Great Idea",
+    desc: "Fully responsive, pixel-perfect website created from photoshop mockups",
+    deployed: "https://blayzestone.github.io/responsive-web-design-I/",
+    github: "https://github.com/blayzestone/responsive-web-design-I",
   },
 ];
