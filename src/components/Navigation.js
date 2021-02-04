5592946bffe3f86c798ec792088d9136279e7cd4
Blayze <blayzestone@protonmail.com>
2021-02-04 00:07:19 -0800

    remove old nav functionality from Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ToggleDarkMode from "./ToggleDarkMode";
 import { useStyles } from "../hooks/styles/useStyles";
 import { links } from "../constants";
 
 const Navigation = ({ darkMode, setDarkMode, setSection }) => {
-  const [targetLink, setTargetLink] = useState(null);
   const { header, navigation } = useStyles();
-
-  useEffect(() => {
-    const element = document.getElementById(targetLink);
-    targetLink && element && element.scrollIntoView({ behavior: "smooth" });
-  }, [targetLink]);
-
-  const scrollToTarget = (evt, link) => {
-    evt.preventDefault();
-    setTargetLink(link);
-  };
 
   return (
     <header className={header}>
       <div className={navigation}>
         {Object.values(links).map((link, i) => (
-          // <a href={`#${link}`} onClick={(evt) => scrollToTarget(evt, link)}>
-          //   {link}
-          // </a>
-          <a href={`#${link}`} onClick={(evt) => setSection(i)}>
+          <a href={`#${link}`} onClick={() => setSection(i)}>
             {link}
           </a>
         ))}
